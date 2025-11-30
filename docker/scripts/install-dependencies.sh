98e118b096a6236ff8e10481c470b69995313ce9
Charles Cui <alavin.cui@gmail.com>
2025-11-30 19:59:41 +0800

    Update install-dependencies.sh

--- docker/scripts/install-dependencies.sh
+++ docker/scripts/install-dependencies.sh
@@ -137,7 +137,7 @@ git clone https://github.com/windytan/redsea.git
 pushd redsea
 # latest from master as of 2024-01-18
 #git checkout c6e6b47ac2c7a9aac9409483b00ca61cd6eb47bd
-git checkout 1.2.0
+git checkout v1.2.0 # 2025.11.30
 ./autogen.sh
 ./configure
 make
