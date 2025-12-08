9e9567992e4dc6cb634c3ffd6d246a881085c709
Charles Cui <alavin.cui@gmail.com>
2025-12-08 20:06:15 +0800

    Update direwolf.py

--- owrx/aprs/direwolf.py
+++ owrx/aprs/direwolf.py
@@ -159,8 +159,8 @@ class DirewolfModule(ExecModule, DirewolfConfigSubscriber):
         self.__writeConfig()
 
         # super().__init__(Format.SHORT, Format.CHAR, ["direwolf", "-c", self.direwolfConfigPath, "-r", "48000", "-t", "0", "-q", "d", "-q", "h"])
-        # 增加FEC参数，采样率改为24k,静默设置修改
-        super().__init__(Format.SHORT, Format.CHAR, ["direwolf", "-c", self.direwolfConfigPath, "-r", "24000", "-t", "0", "-q", "d", "-X", "1"])
+        # 采样率改为24k,静默设置修改
+        super().__init__(Format.SHORT, Format.CHAR, ["direwolf", "-c", self.direwolfConfigPath, "-r", "24000", "-t", "0", "-q", "d"])
 
         # direwolf supplies the data via a socket which we tap into in start()
         # the output on its STDOUT is informative, but we still want to log it
